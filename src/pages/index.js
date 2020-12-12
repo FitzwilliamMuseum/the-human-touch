7e52e130af67df59c80d17e7fbd05e4ba1b9ae5a
Daniel Pett <dejp3@cam.ac.uk>
2020-12-12 01:35:13 +0000

    Move logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,11 @@ import { Link } from "gatsby"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fv-logo-white.svg" alt="FitzVirtual Logo" width="200" />
     <h1 className="cover-heading" style={{fontSize: `4rem`}}>The Human Touch</h1>
     <p className="lead cover">Making Art | Leaving Traces</p>
     <Button variant="btnBook"><Link to="https://tickets.museums.cam.ac.uk/overview/thehumantouch">Book now</Link></Button>
     <p className="lead cover">6 January 2021 to 3 May 2021</p>
-    <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fv-logo-white.svg" alt="FitzVirtual Logo" width="200" />
   </Layout>
 )
 
