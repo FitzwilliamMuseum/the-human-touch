5f6f812399c866c254651b5e1208f86a8d49081f
Daniel Pett <dejp3@cam.ac.uk>
2020-12-15 21:30:51 +0000

    Add logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     description="The background to the Fitzwilliam Museum's brand new exhibition, the Human Touch"
     />
     <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fv-logo-white.svg" alt="FitzVirtual Logo" width="200" />
+    <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitztht.png" alt="Fitzwilliam Museum" width="150">
     <h1 className="cover-heading" style={{fontSize: `4rem`}}>The Human Touch</h1>
     <p className="lead cover">Making Art | Leaving Traces</p>
     <Button variant="btnBook"><Link to="https://tickets.museums.cam.ac.uk/overview/thehumantouch">Book now</Link></Button>
