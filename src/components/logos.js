10b492dad7f4a03adf56f77681d22b406fa140eb
Daniel Pett <dejp3@cam.ac.uk>
2020-12-15 17:20:51 +0000

    Added partner names

--- src/components/logos.js
+++ src/components/logos.js
@@ -15,16 +15,14 @@ render () {
 
   return (
 
-<section className="content_31 bg-dark pb-5 text-center color-white mb-3 mt-30 p-5">
+<section className="content_31 bg-dark pb-3 text-center color-white mb-3 mt-30 p-5">
   <div className="container px-xl-0">
     <div className="row justify-content-center">
-
       <div className="col-lg-3 col-md-5 col-sm-6 aos-init aos-animate mb-3" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
         <div className="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
           <a href="https://beta.fitz.ms"><img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitztht.png" alt="Fitzwilliam Museum" width="150"/></a>
         </div>
       </div>
-
       <div className="col-lg-3 col-md-5 col-sm-6 aos-init aos-animate mb-3" data-aos-duration="600" data-aos="fade-down" data-aos-delay="450">
         <div className="mx-auto mb-30 mw-270 h-160 d-flex justify-content-center align-items-center radius6 block">
           <a href="https://cam.ac.uk"><img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/reversecam.png" alt="FitzVirtual Logo" width="300" className="pt-3"/></a>
@@ -32,6 +30,12 @@ render () {
       </div>
 
 
+    </div>
+    <div className="row justify-content-center">
+      <p className="text-center">
+      Fitzwilliam Museum Business Partners<br/>
+      TTP    |   Brewin Dolphin
+      </p>
     </div>
   </div>
 </section>
