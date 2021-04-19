7ef6dfc5d3073f0127b533ae2dba98b0aaeb40c5
unknown <george@PC205.fitzmuseum.cam.ac.uk>
2021-04-19 21:45:15 +0100

    Updated booking link for reopening

--- src/components/topPanel.js
+++ src/components/topPanel.js
@@ -12,7 +12,7 @@ export default class TopPanel extends Component {
         <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitztht.png" alt="Fitzwilliam Museum" width="150" />
         <h1 className="cover-heading" style={{fontSize: `4rem`}}>The Human Touch</h1>
         <p className="lead cover">Making Art | Leaving Traces</p>
-        <Button variant="btnBook"><Link to="https://tickets.museums.cam.ac.uk/account/create">Sign up for opening news</Link></Button>
+        <Button variant="btnBook"><Link to="https://tickets.museums.cam.ac.uk/overview/thehumantouch">Book your tickets</Link></Button>
       </>
     )
   }
