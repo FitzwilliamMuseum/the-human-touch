ff43bac78a5bf4816a3d7d60193088d967fa226f
Daniel Pett <dejp3@cam.ac.uk>
2021-06-04 19:18:03 +0100

    Updates

--- component---src-pages-objects-js-e4fe8695146700a0ba9b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{vJFS:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return u}));var a=t("q1tI"),c=t.n(a),l=t("rui/"),r=t("Bl7J");function u(){return c.a.createElement(r.a,null,c.a.createElement("div",null,c.a.createElement("h1",null,"Objects"),c.a.createElement("p",null,"Such wow. Very React.")),c.a.createElement(l.a,{src:"/human.mp4"}))}}}]);
-//# sourceMappingURL=component---src-pages-objects-js-e4fe8695146700a0ba9b.js.map
