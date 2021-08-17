f321fbf792d64008ec8da9e2fb42fe51462439e8
Daniel Pett <dejp3@cam.ac.uk>
2021-08-17 21:15:59 +0100

    Remove booking link

--- src/components/topPanel.js
+++ src/components/topPanel.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from "gatsby"
-import Button from 'react-bootstrap/Button';
 
 export default class TopPanel extends Component {
 
@@ -10,9 +9,7 @@ export default class TopPanel extends Component {
         <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitztht.png" alt="Fitzwilliam Museum" width="150" />
         <h1 className="cover-heading" style={{fontSize: `4rem`}}>The Human Touch</h1>
         <p className="lead cover">Making Art | Leaving Traces</p>
-        <Button variant="btnBook"><Link to="https://tickets.museums.cam.ac.uk/overview/thehumantouch">Book your tickets</Link></Button>
-        <p className="lead cover">18 May 2021 to 1 August 2021</p>
-
+        <p className="lead cover">This exhibition ran from the 18 May 2021 to 1 August 2021</p>
       </>
     )
   }
